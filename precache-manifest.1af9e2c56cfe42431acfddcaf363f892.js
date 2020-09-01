274188978b30489558329d415ba661aa386b55f4
Nithin <nithin.code1@gmail.com>
2020-09-01 12:19:12 +0530

    Updates

---
+++ precache-manifest.1af9e2c56cfe42431acfddcaf363f892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a22d9748ca12f6172b44f10418b00f",
+    "url": "/to-do-simple/index.html"
+  },
+  {
+    "revision": "5d3c6e3f18ba8937e60f",
+    "url": "/to-do-simple/static/css/main.dd3a2d97.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/to-do-simple/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "5d3c6e3f18ba8937e60f",
+    "url": "/to-do-simple/static/js/main.0e670121.chunk.js"
+  },
+  {
+    "revision": "3effe9ac4eec19383f43",
+    "url": "/to-do-simple/static/js/runtime~main.91f7c6a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-simple/static/media/logo.5d5d9eef.svg"
+  }
+]);
